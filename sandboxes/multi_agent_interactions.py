95019b6d1349627ec4e9a8c81f701ea0ede683e7
Dallin Cordon <cordond@Cordon-MacBook-Pro.local>
2024-01-22 21:07:10 -0700

    removed instances of perpendicular_projection_net after proving it doesn't work

--- sandboxes/multi_agent_interactions.py
+++ sandboxes/multi_agent_interactions.py
@@ -1,8 +1,9 @@
 #%% Imports
+import sys
+sys.path.append('..')  # Add the parent directory to the Python path
 import numpy as np
-import matplotlib.pyplot as plt
 from interaction_eqs import *
-from ..tools.vector_utils import *
+from tools.vector_utils import *
 
 #==============================================================================
 #%%
@@ -59,34 +60,56 @@ print(f"|F_net + Fi_ab_net*2| = {combine_net}")
 
 #==============================================================================
 #%%
+# Two agents pull 180 degrees apart with same force
 Fa = np.array([3,0])
-Fb = np.array([-2,0])
-Fc = polar_to_cartesian(1, np.pi/4)
+Fb = polar_to_cartesian(2, np.pi-0.001)
 
-F_net = Fa + Fb + Fc
+Fnet = Fa + Fb
+Fi_ab_bi = perpendicular_projection_bisection(Fa,Fb)
+Fi_ab_net = perpendicular_projection_net(Fa,Fb)
+equivalency = np.array_equal(Fi_ab_bi,Fi_ab_net)
 
-Fi_ab = perpendicular_projection_bisection(Fa,Fb)
-Fi_ac = perpendicular_projection_bisection(Fa,Fc)
-Fi_bc = perpendicular_projection_bisection(Fb,Fc)
+combine_bi = get_magnitude(Fnet + Fi_ab_bi*2)
+combine_net = get_magnitude(Fnet + Fi_ab_net*2)
 
-Fi_net = Fi_ab + Fi_ac + Fi_bc
+colors = ['r','b','k','m','c']
+vectors = np.array([Fa,Fb,Fnet,Fi_ab_bi,Fi_ab_net])
 
-combine = F_net + Fi_net*2
-
-colors = ['r','g','b','c','m','y','khaki','k']
-
-vectors = np.array([Fa,Fb,Fc,Fi_ab,Fi_ac,Fi_bc,Fi_net,F_net])
 plot_vectors(vectors, colors)
-
-print(f"|Fa| = {np.linalg.norm(Fa)}")
-print(f"|Fb| = {np.linalg.norm(Fb)}")
-print(f"|Fc| = {np.linalg.norm(Fc)}")
-print(f"|Fi_ab| = {np.linalg.norm(Fi_ab)}")
-print(f"|Fi_ac| = {np.linalg.norm(Fi_ac)}")
-print(f"|Fi_bc| = {np.linalg.norm(Fi_bc)}")
-print(f"|F_net| = {np.linalg.norm(Fnet)}")
-print(f"|Fi_net| = {np.linalg.norm(Fi_ab)}")
-print(f"|F_net + Fi*2| = {np.linalg.norm(combine)}")
+print(f"|Fa| = {get_magnitude(Fa)}")
+print(f"|Fb| = {get_magnitude(Fb)}")
+print(f"|F_net| = {get_magnitude(Fnet)}")
+print(f"Bisection = Net?: {equivalency}")
+print(f"|Fi_ab_bi| = {get_magnitude(Fi_ab_bi)}")
+print(f"|Fi_ab_net| = {get_magnitude(Fi_ab_net)}")
+print(f"|F_net + Fi_ab_bi*2| = {combine_bi}")
+print(f"|F_net + Fi_ab_net*2| = {combine_net}")
 
 #==============================================================================
+# %%
+# Two agents pull together degrees apart with same force
+Fa = np.array([2,0])
+Fb = np.array([2,0])
+
+Fnet = Fa + Fb
+Fi_ab_bi = perpendicular_projection_bisection(Fa,Fb)
+Fi_ab_net = perpendicular_projection_net(Fa,Fb)
+equivalency = np.array_equal(Fi_ab_bi,Fi_ab_net)
+
+combine_bi = get_magnitude(Fnet + Fi_ab_bi*2)
+combine_net = get_magnitude(Fnet + Fi_ab_net*2)
+
+colors = ['r','b','k','m','c']
+vectors = np.array([Fa,Fb,Fnet,Fi_ab_bi,Fi_ab_net])
+
+plot_vectors(vectors, colors)
+print(f"|Fa| = {get_magnitude(Fa)}")
+print(f"|Fb| = {get_magnitude(Fb)}")
+print(f"|F_net| = {get_magnitude(Fnet)}")
+print(f"Bisection = Net?: {equivalency}")
+print(f"|Fi_ab_bi| = {get_magnitude(Fi_ab_bi)}")
+print(f"|Fi_ab_net| = {get_magnitude(Fi_ab_net)}")
+print(f"|F_net + Fi_ab_bi*2| = {combine_bi}")
+print(f"|F_net + Fi_ab_net*2| = {combine_net}")
+
 # %%
